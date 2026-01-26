8e1c9adea0c28fb2d2b75d3b347e1191f2e1d2f9
galasky05 <143463688+galasky05@users.noreply.github.com>
2026-01-26 23:56:20 +0700

    Create Product Details Page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -13,4 +13,25 @@ class ShopController extends Controller
         $product = Product::orderBy('created_at','DESC')->paginate(12);
         return view('shop',['products'=>$product]);
     }
+
+    public function productDetails($slug)
+{
+    $product = Product::where('slug', $slug)->first();
+
+    if (!$product) {
+        abort(404);
+    }
+
+    $rproducts = Product::where('slug', '!=', $slug)
+                    ->inRandomOrder()
+                    ->take(8)
+                    ->get();
+
+    return view('details', [
+        'product'   => $product,
+        'rproducts' => $rproducts
+    ]);
 }
+
+}   
+
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[AppController::class,'index'])->name('app.index');
 Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
+Route::get('/product/{slug}', [ShopController::class, 'productDetails'])->name('shop.product.details');
 Auth::routes();
 
 Route::middleware('auth')->group(function(){
